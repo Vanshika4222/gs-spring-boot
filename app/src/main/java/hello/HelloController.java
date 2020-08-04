7fa27382fc2d54938e7e4e9740aa05832d260258
Vanshika4222 <67817404+Vanshika4222@users.noreply.github.com>
2020-08-04 16:40:47 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello Vanshika from Spring Boot!";
+        return "Hello from Spring Boot!";
     }   
 }
